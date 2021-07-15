bd2bf7a2466e72f9e79967b18b02676743dbdaa9
Ezra Stanley <ezrastanley09@gmail.com>
2021-07-15 15:42:39 -0400

    fetch api data

--- js/api.js
+++ js/api.js
@@ -1,2 +1,12 @@
 const apiKey = "UtGdsg8vwWKwaIEmaMuyCjdqszb5tz9s"
-let url = `https://api.windy.com/api/webcams/v2/list?show=webcams:title;image,location;categories;player&key=${apiKey}`
+let url = `https://api.windy.com/api/webcams/v2/list?show=webcams:title;image;regions,location;categories;player&key=${apiKey}`
+
+function apiFunction() {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+apiFunction ();
