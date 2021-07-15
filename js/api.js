c62cd8331f7789686a4873b7e39382cf67f1f308
Ezra Stanley <ezrastanley09@gmail.com>
2021-07-15 15:23:44 -0400

    fix api url

--- js/api.js
+++ js/api.js
@@ -1,3 +1,2 @@
 const apiKey = "UtGdsg8vwWKwaIEmaMuyCjdqszb5tz9s"
-let url = "https://api.windy.com/api/webcams/v2/list?show=webcams:image,location;categories&?key=apiKey"
-
+let url = `https://api.windy.com/api/webcams/v2/list?show=webcams:title;image,location;categories;player&key=${apiKey}`
